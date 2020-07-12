9b5eabdef120a1a9acf6d537e60454ca063b06df
Sowmica M.L <sowmica34@yahoo.com>
2020-07-12 11:36:00 +0530

    Create subset.py

---
+++ Subsets/subset.py
@@ -0,0 +1,26 @@
+********************************************************************
+Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+Example:
+Input: nums = [1,2,3]
+Output:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+********************************************************************
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = [[]]
+        for n in nums:
+            for i in result:
+                result = result +[i + [n]]
+        return result
+        
